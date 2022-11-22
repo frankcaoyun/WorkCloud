4f5f46b1558692363c16f2337eb8c60e3f059402
frank_cy <frank_cy@aiap.sg>
2022-11-22 09:14:08 +0800

    issue: not able to retrieve the target field value

--- src/scrapper.py
+++ src/scrapper.py
@@ -1,8 +1,10 @@
 # Reference: https://maoviola.medium.com/a-complete-guide-to-web-scraping-linkedin-job-postings-ad290fcaa97f
 
-from selenium import webdriver
 import time
 import pandas as pd
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 # define the url for scraping
 url = 'https://www.linkedin.com/jobs/search?keywords=Machine%20Learning%20Engineer&location=Singapore%2C%20Singapore&geoId=&trk=homepage-jobseeker_jobs-search-bar_search-submit&position=1&pageNum=0'
@@ -12,7 +14,10 @@ wd = webdriver.Chrome(executable_path='./chromedriver.exe')
 wd.get(url)
 
 # get the number of jobs
-no_of_jobs = int(wd.find_element(
-    'h1>span').get_attribute('innerText'))
+# no_of_jobs = wd.find_element(By.CSS_SELECTOR, 'span.results-context-header__new-jobs`')
+no_of_jobs = wd.find_element(By.CSS_SELECTOR, 'h1>span')
+print('test: ', no_of_jobs.get_attribute(name='class'))
+# print('test: ', no_of_jobs.getText())
 
-print(no_of_jobs)
+print('the number of jobs are: \n', no_of_jobs)
+
